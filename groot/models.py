d2ac6d2c5b06fccce0dfe824f67dd00955fd162b
gkgk6677 <gkgk6677@naver.com>
2018-12-27 17:02:20 +0900

    Database 수정(Enrollment, SortMST 테이블 추가 및 Cert_vf 테이블 제거"

--- groot/models.py
+++ groot/models.py
@@ -4,23 +4,13 @@ from django.db import models
 from django.forms import SelectDateWidget
 
 
-class CertVf(models.Model):
-    cert_idx = models.AutoField(primary_key=True)
-    pre_hash = models.CharField(max_length=100, blank=True, null=True)
-    up_hash = models.CharField(max_length=100, blank=True, null=True)
-    c_date = models.DateTimeField()
-    certificate_issu_idx = models.ForeignKey('Certificate', models.DO_NOTHING, db_column='certificate_issu_idx')
-
-    class Meta:
-        managed = False
-        db_table = 'Cert_vf'
-
-
 class Certificate(models.Model):
-    issu_idx = models.AutoField(primary_key=True)
-    e_date = models.DateTimeField()
+    cert_idx = models.AutoField(primary_key=True)
+    enroll_idx = models.ForeignKey('Enrollment', models.DO_NOTHING, db_column='enroll_idx')
+    term = models.IntegerField()
+    end_date = models.DateTimeField()
+    cert_tx = models.CharField(max_length=100)
     c_date = models.DateTimeField()
-    contract_cont_idx = models.ForeignKey('Contract', models.DO_NOTHING, db_column='contract_cont_idx')
 
     class Meta:
         managed = False
@@ -29,38 +19,55 @@ class Certificate(models.Model):
 
 class Contract(models.Model):
     cont_idx = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100)
-    sort = models.CharField(max_length=100)
-    e_date = models.DateTimeField()
-    c_date = models.DateTimeField(auto_now=True)
-    user_user= models.ForeignKey('User', models.DO_NOTHING)
+    enroll_idx = models.ForeignKey('Enrollment', models.DO_NOTHING, db_column='enroll_idx')
+    user_id = models.ForeignKey('User', models.DO_NOTHING)
+    term = models.IntegerField()
+    end_date = models.DateTimeField()
+    c_date = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'Contract'
+        unique_together = (('cont_idx', 'user', 'enroll_idx'),)
 
 
 class DocVf(models.Model):
     doc_idx = models.AutoField(primary_key=True)
-    pre_hash = models.CharField(max_length=100, blank=True, null=True)
-    up_hash = models.CharField(max_length=100, blank=True, null=True)
+    uuid = models.ForeignKey('File', models.DO_NOTHING, db_column='uuid')
+    up_hash = models.CharField(max_length=100)
     c_date = models.DateTimeField()
-    file_file_idx = models.ForeignKey('File', models.DO_NOTHING, db_column='file_file_idx')
 
     class Meta:
         managed = False
         db_table = 'Doc_vf'
+        unique_together = (('doc_idx', 'uuid'),)
+
+
+class Enrollment(models.Model):
+    enroll_idx = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('User', models.DO_NOTHING)
+    sort_idx = models.ForeignKey('SortMst', models.DO_NOTHING, db_column='sort_idx')
+    title = models.CharField(max_length=100)
+    term = models.IntegerField()
+    enroll_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    enroll_tx = models.CharField(max_length=100, blank=True, null=True)
+    c_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'Enrollment'
+        unique_together = (('enroll_idx', 'user', 'sort_idx'),)
 
 
 class File(models.Model):
-    file_idx = models.AutoField(primary_key=True)
-    uuid = models.CharField(max_length=100)
+    uuid = models.CharField(primary_key=True, max_length=100)
+    enroll_idx = models.ForeignKey(Enrollment, models.DO_NOTHING, db_column='enroll_idx')
     file_name = models.CharField(max_length=100)
     file_type = models.CharField(max_length=100)
     file_hash = models.CharField(max_length=100)
-    s_date = models.DateTimeField()
+    m_date = models.DateTimeField()
     c_date = models.DateTimeField()
-    contract_cont_idx = models.ForeignKey(Contract, models.DO_NOTHING, db_column='contract_cont_idx')
 
     class Meta:
         managed = False
@@ -69,47 +76,38 @@ class File(models.Model):
 
 class ItBoard(models.Model):
     board_idx = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('User', models.DO_NOTHING)
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, null=True)
     count = models.IntegerField(blank=True, null=True)
     c_date = models.DateTimeField()
-    user_user = models.ForeignKey('User', models.DO_NOTHING)
+    m_date = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'It_board'
+        unique_together = (('board_idx', 'user'),)
 
 
 class Notice(models.Model):
     notice_idx = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('User', models.DO_NOTHING)
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, null=True)
     count = models.IntegerField(blank=True, null=True)
-    c_date = models.DateTimeField(auto_now_add=True)
-    user_user = models.ForeignKey('User', models.DO_NOTHING)
-
-
+    c_date = models.DateTimeField()
+    m_date = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'Notice'
-
-    # @@2018 12 18 추가@@
-    def __str__(self):
-        return self.title
+        unique_together = (('notice_idx', 'user'),)
 
 
-class User(models.Model):
-    user_id = models.CharField(primary_key=True, max_length=100)
-    user_pw = models.CharField(max_length=100)
-    com_num = models.IntegerField()
-    com_name = models.CharField(max_length=100)
-    com_head = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    phone_num = models.IntegerField()
-    c_date = models.DateTimeField()
+class SortMst(models.Model):
+    sort_idx = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
 
     class Meta:
         managed = False
-        db_table = 'User'
+        db_table = 'Sort_MST'
