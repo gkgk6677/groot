883f0546c7d35042362ba96cd04bb3581c26ef87
seo <gkgk6677@naver.com>
2019-01-18 16:05:16 +0900

    수정된 DB에 따라 Admin status관련 코드 수정

--- administrator/views.py
+++ administrator/views.py
@@ -7,7 +7,11 @@ from groot.models import *
 
 def index(request):
 
-    status = Status.objects.all()
+    enroll_status = Enrollment.objects.all()
+    contract_status = Contract.objects.all()
+    extend_status = Extend.objects.all()
+    update_status = Update.objects.all()
+    expire_status = Expire.objects.all()
 
     count_enroll = 0
     count_cont = 0
@@ -15,19 +19,23 @@ def index(request):
     count_update = 0
     count_expire = 0
 
-    for val in status:
+    for val in enroll_status:
         if val.enroll_status == 0:
             count_enroll += 1
-        if val.contract_status == 0 and val.enroll_status == 1:
+    for val in contract_status:
+        if val.status == 0:
             count_cont += 1
-        if val.extend_status == 0 and val.enroll_status == 1:
+    for val in extend_status:
+        if val.status == 0:
             count_extend += 1
-        if val.update_status == 0 and val.enroll_status == 1:
+    for val in update_status:
+        if val.status == 0:
             count_update += 1
-        if val.expire_status == 0 and val.enroll_status == 1:
+    for val in expire_status:
+        if val.status == 0:
             count_expire += 1
 
-    return render(request, 'administrator/index.html', {'status':status,   'count_enroll':count_enroll, 'count_cont':count_cont, 'count_extend':count_extend, 'count_update':count_update, 'count_expire':count_expire})
+    return render(request, 'administrator/index.html', {'count_enroll':count_enroll, 'count_cont':count_cont, 'count_extend':count_extend, 'count_update':count_update, 'count_expire':count_expire})
 
 def logout(request):
     del request.session['user_id']
@@ -37,7 +45,7 @@ def blank(request):
     return render(request, 'administrator/blank.html', {})
 
 def admin_application(request):   
-    app_info = Status.objects.all().filter(enroll_status=0)
+    app_info = Enrollment.objects.all().filter(enroll_status=0)
 
     if request.method == 'GET':
         return render(request, 'administrator/admin-application.html', {'app_info':app_info})
@@ -46,16 +54,15 @@ def admin_application(request):
 def application_detail(request, idx):
 
     enrollment_info = Enrollment.objects.get(enroll_idx=idx)
-    status_info = Status.objects.get(enroll_idx=idx)
+    # status_info = Status.objects.get(enroll_idx=idx)
 
     if request.method == 'GET': 
         return render(request, 'administrator/application_detail.html', {'enrollment_info':enrollment_info})
     else:
         if request.POST.get('yes'):
-            status_info.enroll_status = 1
+            enrollment_info.enroll_status = 1
             enrollment_info.enroll_date = datetime.datetime.now()
             enrollment_info.end_date = datetime.datetime.now() + + datetime.timedelta(days=(365 * int(enrollment_info.term)))
-            status_info.save()
 
             #     0          1        2         3        4        5       6          7            8          9
             # Technology   Sort   Company   Com_num   Term   Content   Client   Cont_term   Enroll_date   Status
@@ -72,9 +79,8 @@ def application_detail(request, idx):
         elif request.POST.get('check'):
             return redirect('admin_application')
         else:
-            status_info.enroll_status = 2
+            enrollment_info.enroll_status = 2
             enrollment_info.enroll_date = datetime.datetime.now()
-            status_info.save()
             enrollment_info.save()
             return redirect('index')
 
